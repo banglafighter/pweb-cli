296dd5021ff17e0c107e9fbe38c188c6eb04563b
hmtmcse <hmtm.cse@gmail.com>
2023-10-28 14:40:56 +0600

    re export

--- pweb_cli/__init__.py
+++ pweb_cli/__init__.py
@@ -4,3 +4,4 @@ from pweb_cli.common.pweb_source_man import PWebSourceMan
 from pweb_cli.common.pweb_cli_named import ActionStatus
 from pweb_cli.data.pweb_cli_pwebsm import PWebSMClone, PWebSMRepo
 from pweb_cli.data.pweb_cli_pwebsm import PWebSMDependency
+from pweb_cli.module.pweb_cli_module_man import PWebCLIModuleMan
