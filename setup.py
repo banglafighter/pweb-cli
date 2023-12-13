ee1510f837cd7ac13456f832b709dabc53dbc36c
Touhid Mia <touhid@banglafighter.com>
2023-12-13 19:23:09 +0600

    Release New Version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_dependencies():
 
 setup(
     name='pweb-cli',
-    version='0.0.14',
+    version='0.0.15',
     url='https://github.com/banglafighter/pweb-cli',
     license='Apache 2.0',
     author='Bangla Fighter',
